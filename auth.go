0903e3a8189ee2508114f2a860f5d90b30ffff71
Steven Penny <srpen6@gmail.com>
2021-11-05 22:42:55 -0500

    new file:   cmd/googleplay/main.go

--- auth.go
+++ auth.go
@@ -6,7 +6,7 @@ import (
    "io"
    "net/http"
    "net/url"
-   "strings"
+   "strconv"
    "time"
 )
 
@@ -15,51 +15,49 @@ const (
    agent = "Android-Finsky (sdk=99,versionCode=99999999)"
 )
 
-type AppDetails struct {
-   DeveloperName string `protobuf:"bytes,1"`
-   VersionCode int32 `protobuf:"varint,3"`
-   Version string `protobuf:"bytes,4"`
-   InstallationSize int64 `protobuf:"varint,9"`
-   DeveloperEmail string `protobuf:"bytes,11"`
-}
-
 type Auth struct {
    url.Values
 }
 
-func (a Auth) Purchase(deviceID, app string) ([]byte, error) {
-   buf := url.Values{
-      "doc": {app},
-   }.Encode()
-   req, err := http.NewRequest(
-      "POST", origin + "/fdfe/purchase", strings.NewReader(buf),
-   )
+func (a Auth) Delivery(dID, app string, ver int) (*Delivery, error) {
+   req, err := http.NewRequest("GET", origin + "/fdfe/delivery", nil)
    if err != nil {
       return nil, err
    }
    req.Header = http.Header{
       "Authorization": {"Bearer " + a.Get("Auth")},
-      "Content-Type": {"application/x-www-form-urlencoded"},
       "User-Agent": {agent},
-      "X-DFE-Device-ID": {deviceID},
+      "X-DFE-Device-ID": {dID},
    }
+   req.URL.RawQuery = url.Values{
+      "doc": {app},
+      "vc": {strconv.Itoa(ver)},
+   }.Encode()
    res, err := roundTrip(req)
    if err != nil {
       return nil, err
    }
    defer res.Body.Close()
-   return io.ReadAll(res.Body)
+   buf, err := io.ReadAll(res.Body)
+   if err != nil {
+      return nil, err
+   }
+   var wrap responseWrapper
+   if err := proto.Unmarshal(buf, &wrap); err != nil {
+      return nil, err
+   }
+   return &wrap.Payload.DeliveryResponse, nil
 }
 
-// deviceID is Google Service Framework.
-func (a Auth) Details(deviceID, app string) (*AppDetails, error) {
+// `dID` is Google Service Framework.
+func (a Auth) Details(dID, app string) (*Details, error) {
    req, err := http.NewRequest("GET", origin + "/fdfe/details", nil)
    if err != nil {
       return nil, err
    }
    req.Header = http.Header{
       "Authorization": {"Bearer " + a.Get("Auth")},
-      "X-DFE-Device-ID": {deviceID},
+      "X-DFE-Device-ID": {dID},
    }
    req.URL.RawQuery = url.Values{
       "doc": {app},
@@ -77,15 +75,15 @@ func (a Auth) Details(deviceID, app string) (*AppDetails, error) {
    if err := proto.Unmarshal(buf, &wrap); err != nil {
       return nil, err
    }
-   return wrap.appDetails(), nil
+   return &wrap.Payload.DetailsResponse, nil
 }
 
 // This seems to return `StatusOK`, even with invalid requests, and the response
 // body only contains a token, that doesnt seem to indicate success or failure.
-// Only way I know to check, it to try the `deviceID` with a `details` request
-// or similar. Also, after the POST, you need to wait at least 16 seconds
-// before the `deviceID` can be used.
-func (a Auth) Upload(deviceID string, dev Device) error {
+// Only way I know to check, it to try the `dID` with a `details` request or
+// similar. Also, after the POST, you need to wait at least 16 seconds before
+// the `dID` can be used.
+func (a Auth) Upload(dID string, dev Device) error {
    buf, err := proto.Marshal(dev)
    if err != nil {
       return err
@@ -99,7 +97,7 @@ func (a Auth) Upload(deviceID string, dev Device) error {
    req.Header = http.Header{
       "Authorization": {"Bearer " + a.Get("Auth")},
       "User-Agent": {agent},
-      "X-DFE-Device-ID": {deviceID},
+      "X-DFE-Device-ID": {dID},
    }
    res, err := roundTrip(req)
    if err != nil {
@@ -108,18 +106,29 @@ func (a Auth) Upload(deviceID string, dev Device) error {
    return res.Body.Close()
 }
 
-type responseWrapper struct {
-   Payload struct {
-      DetailsResponse struct {
-         DocV2 struct {
-            DocumentDetails struct {
-               AppDetails AppDetails `protobuf:"bytes,1"`
-            } `protobuf:"bytes,13"`
-         } `protobuf:"bytes,4"`
-      } `protobuf:"bytes,2"`
-   } `protobuf:"bytes,1"`
+type Delivery struct {
+   AppDeliveryData struct {
+      DownloadUrl string `protobuf:"bytes,3"`
+   } `protobuf:"bytes,2"`
 }
 
-func (r responseWrapper) appDetails() *AppDetails {
-   return &r.Payload.DetailsResponse.DocV2.DocumentDetails.AppDetails
+type Details struct {
+   DocV2 struct {
+      DocumentDetails struct {
+         AppDetails struct {
+            DeveloperName string `protobuf:"bytes,1"`
+            VersionCode int `protobuf:"varint,3"`
+            Version string `protobuf:"bytes,4"`
+            InstallationSize int `protobuf:"varint,9"`
+            DeveloperEmail string `protobuf:"bytes,11"`
+         } `protobuf:"bytes,1"`
+      } `protobuf:"bytes,13"`
+   } `protobuf:"bytes,4"`
+}
+
+type responseWrapper struct {
+   Payload struct {
+      DetailsResponse Details `protobuf:"bytes,2"`
+      DeliveryResponse Delivery `protobuf:"bytes,21"`
+   } `protobuf:"bytes,1"`
 }
